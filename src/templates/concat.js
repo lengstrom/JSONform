014636f19276614b4e1d367549d314bee4f822ad
Logan Engstrom <meadowstream@users.noreply.github.com>
2014-08-31 00:26:07 -0400

    added concat.js

---
+++ src/templates/concat.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var path = require('path')
+var total = {}
+dirList = fs.readdirSync(__dirname);
+for (var i in dirList) {
+    if (dirList[i].indexOf('.html') > -1) total[dirList[i].slice(0, -5)] = fs.readFileSync(path.join(__dirname, dirList[i])).toString();
+}
+
+fs.writeFileSync(__dirname + "/bootstrap_components.json", JSON.stringify(total))
