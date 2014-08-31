bd55fe7b11283c3d71c090d2b034bf46c9d3a603
Noah Moroze <noahm@moroze.com>
2014-08-31 00:12:11 -0400

    Added underscore

--- src/JSONForm.js
+++ src/JSONForm.js
@@ -1,14 +1,7 @@
 function JSONForm(input) {
-    this.plugins = {
-        "text": 
-        "textbox":
-        "radio": 
-        "submit":
-    };
-
     this.init = function(input, opts) {
         this.sourceObject = input;
-        this.classes = opts.classes || {/*bootstrap classes*/}; 
+        this.classes = opts.classes || {}; 
     }
     
     this.render = function() {
@@ -21,6 +14,26 @@ function JSONForm(input) {
     }
     
     this.processRow = function(rowSource) {
-        return this.plugins[rowSource.type](rowSource);
+        return this.fillTemplate(templates[rowSource["type"]], rowSource);
+    }
+
+    this.fillTemplate = function(template, data) {
+        var filledTemplate = "";
+        var prevIndex = 0;
+        var index = template.indexOf("%");
+
+        while(index != -1) {
+            filledTemplate += template.substring(prevIndex, index); 
+            prevIndex = index + 1;
+            index = template.indexOf("%", prevIndex);
+            
+            var label = template.substring(prevIndex, index);
+            filledTemplate += data[label] || ""; // empty string if no matching label. should this throw an error?
+            
+            prevIndex = index + 1;
+            index = template.indexOf("%", prevIndex);
+        }
+        filledTemplate += template.substring(prevIndex, template.length); 
+        return filledTemplate;
     }
 }
